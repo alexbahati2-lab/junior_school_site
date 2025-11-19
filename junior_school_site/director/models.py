8e89e41f8b84e8619fe7374814ed32b423fe7b9b
Alex Bahati <alexbahati2@gmail.com>
2025-11-19 15:37:22 +0300

    Initial commit for junior school site

---
+++ junior_school_site/director/models.py
@@ -0,0 +1,39 @@
+
+
+# Create your models here.
+from django.db import models
+
+class HomePage(models.Model):
+    welcome_text = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Home Page Content"
+
+
+class AboutPage(models.Model):
+    mission = models.TextField()
+    vision = models.TextField()
+    history = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "About Page Content"
+
+
+class GalleryImage(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='gallery/')
+    caption = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.date})"
