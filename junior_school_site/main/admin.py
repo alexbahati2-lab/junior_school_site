f83ba38998fcb8d43649ec1b97fac656aaebafed
Alex Bahati <alexbahati2@gmail.com>
2025-11-26 09:25:52 +0300

    Fix: restored deleted project folders and updated settings

--- junior_school_site/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Teacher, News, Gallery
-
-admin.site.register(Teacher)
-admin.site.register(News)
-admin.site.register(Gallery)
--- junior_school_site/settings.py
+++ junior_school_site/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-=khryd4)#htwq9z23n4-g_xp%orxm_2-94g($2w@yi&4n6&=f*'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['junior-school-site-3.onrender.com', 'localhost', '127.0.0.1']
 
 # Installed apps
 INSTALLED_APPS = [
